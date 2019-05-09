cd91e9584bf56e12cf3a1374b1fbdde60ad96272
johncban <johncban@gmail.com>
2019-05-09 03:38:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,10 +124,10 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C", "Assembly"]
         }
      }
 
-     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     programmer_hash[:dennis_ritchie][:languages][2]
 
 end
